6c7020961786c836e115f8a22586b6d39c805055
Duong PY <113834956+duongvodich@users.noreply.github.com>
2022-10-24 10:09:31 +0700

    Update feed_new_pictures.py

--- feed_new_pictures.py
+++ feed_new_pictures.py
@@ -2,7 +2,7 @@ from lib2to3.refactor import get_all_fix_names
 from requests import session
 
 s=session()
-access_token='EAAGNwlgFPQYBAOyGhbAkx4L16Ij9vafCIfanShJ7yS8MLJmcK238gZBep849ttBjTAdIv6opzYs8ZBKfCJHlGC1j1aDxnGoIVhkCGbvVSbboeOJJjddS2RqDY6ZBMk8dpMITwlpc0VuSWWJaOmH1IoWWfqUiQ1dZBBsnTgf1OAj6fDOrWVMVo9wLPPMASj8ZD'
+access_token='Token_Facebook_Fulll'
 id_group = "gentle.fanpage"
 get_api='?fields=feed.limit(100){full_picture}'
 json_data= s.get('https://graph.facebook.com/' + id_group + get_api + '&access_token=' + access_token)
